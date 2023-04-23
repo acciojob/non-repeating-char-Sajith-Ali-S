268086de70c13da74679a1d6637e62fd3e593732
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-04-23 10:03:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function f(str){
+    let len = str.length;
+    let count =1;
+    let i =0;
+    let j =1;
+	while(i<len && j<len){
+        if(str.charAt(i)==str.charAt(j)){
+            j++
+            count++
+        }
+        else{
+            if(count==1)return str.charAt(i)
+            i=j;
+            j= i+1;
+            count=1
+        }
+    }
+}
+
+var str = prompt();
+alert(f(str))
